93acad4e9b46e8f55f59257a54291f8895cb8025
Filip Miladinovic <f.miladinovic@netconomy.net>
2020-12-21 22:35:52 +0100

    Initial code commit

---
+++ Busha/Busha/BushaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BushaApp.swift
+//  Busha
+//
+//  Created by Filip Miladinovic on 21.12.20.
+//
+
+import SwiftUI
+
+@main
+struct BushaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
